fc98ae7c59ca97749aa2453737deb0fcb64fd1a2
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-02 20:39:09 +0000

    correct default values

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,8 +40,7 @@ class Server:
             }
             return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Get hyperdata by index including current start index,
         next index, page size, and actual data for the page.
