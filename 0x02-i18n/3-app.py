b06cdcc51928defd944c270dd852cc92f8d0f207
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-10 01:47:21 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""This models creates a basic Flask App
+   that get locale from request.
+"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Babel Configuration class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Pick the best language translation
+       to use for a request
+    """
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=True)
+def index():
+    """Get user local language"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
